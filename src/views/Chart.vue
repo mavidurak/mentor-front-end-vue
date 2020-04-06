d2a3a2a945828f7597de3afbcce3d6668bf187bc
CSToygun <ibrahim.kavas@ogr.sakarya.edu.tr>
2020-04-06 21:51:35 +0300

    Chart.js eklendi

---
+++ src/views/Chart.vue
@@ -0,0 +1,16 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: ['chartdata', 'options'],
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
