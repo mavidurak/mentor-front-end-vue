e06f983e59adaff85a2466351f2b83bf577b0302
CSToygun <ibrahim.kavas@ogr.sakarya.edu.tr>
2020-04-07 01:53:19 +0300

    Chart.js Güncellendi

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,31 +1,46 @@
 <script>
-import { Bar } from 'vue-chartjs'
-
+import { Line } from 'vue-chartjs'
 export default {
-  extends: Bar,
-  data: () => ({
-    chartdata: {
-      labels: ['January', 'February'],
+  extends: Line, // grafiğin türü belirlenir.
+  mounted () {
+    this.renderChart({
+      labels: ['Huawei', 'Oppo', 'Vestel', 'Xaomi', 'Blackberry', 'Honor', 'General-Mobile', 'Samsung', 'Iphone', 'Realme', 'Nokia'],
       datasets: [
         {
-          label: 'Data One',
-          backgroundColor: '#f87979',
-          data: [25, 20]
+          label: 'Telephone Sales',
+          fill: false,
+          data: [40, 20, 18, 39, 10, 40, 39, 80, 40, 20, 16], // veri girisi yapılır.
+          borderColor: [
+            ' #000000',
+            ' #FFFFFF',
+            ' #000000',
+            ' #FFFFFF',
+            ' #000000',
+            ' #FFFFFF',
+            ' #000000',
+            ' #FFFFFF',
+            ' #000000',
+            ' #FFFFFF',
+            ' #000000'
+          ],
+          backgroundColor: [ // noktaların rengi belirlenir.
+            ' #42A5F5',
+            ' #CC0000',
+            ' #CC00FF',
+            ' #00FF00',
+            ' #42A5F5',
+            ' #3333CC',
+            ' #990000',
+            ' #FF66FF',
+            ' #42A5F5',
+            ' #00FFCC',
+            ' #FF0000'
+          ]
         }
       ]
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false
-    }
-  }),
-
-  mounted () {
-    this.renderChart(this.chartdata, this.options)
+    })
   }
 }
-
 </script>
-
 <style>
 </style>
