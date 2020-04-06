9da34601caed05482ad823ff68d29c47f5fd86e4
CSToygun <ibrahim.kavas@ogr.sakarya.edu.tr>
2020-04-06 22:10:18 +0300

    chart.js eklendi

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,16 +1,31 @@
 <script>
-import { Line } from 'vue-chartjs'
+import { Bar } from 'vue-chartjs'
 
 export default {
-  extends: Line,
-  props: ['chartdata', 'options'],
+  extends: Bar,
+  data: () => ({
+    chartdata: {
+      labels: ['January', 'February'],
+      datasets: [
+        {
+          label: 'Data One',
+          backgroundColor: '#f87979',
+          data: [25, 20]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+
   mounted () {
     this.renderChart(this.chartdata, this.options)
-    
   }
 }
+
 </script>
 
 <style>
-
 </style>
