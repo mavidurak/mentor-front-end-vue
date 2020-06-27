fa73d7e8ee61ace4016b53f54a74cbacb52fa0a1
YusufSina <yusufsinayildiz@gmail.com>
2020-06-27 14:56:28 +0300

    vuetify eklendi, API DAhboard tarafında datatable kullanıldı

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Datatable from 'vue2-datatable-component'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
 import './styles/main.scss'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import './vee-validate'
@@ -13,5 +14,6 @@ Vue.config.productionTip = false
 Vue.use(Datatable) // done!
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/views/APIDashboardVMC/Index.vue
+++ src/views/APIDashboardVMC/Index.vue
@@ -8,39 +8,25 @@
               <h5>Api Uygulamaları</h5>
             </div>
             <div class="col-md-4">
-              <router-link to="/api-dashboard-vmc/add-or-update" class="btn btn-success float-right">Oluştur</router-link>
+              <router-link
+                to="/api-dashboard-vmc/add-or-update"
+                class="btn btn-success float-right"
+              >Oluştur</router-link>
             </div>
           </div>
-          </div>
+        </div>
         <hr />
-        <table class="table table-bordered text-center">
-          <thead>
-            <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Title</th>
-              <th scope="col">Açıklama</th>
-              <th scope="col">Oluşturma Tarihi</th>
-              <th scope="col">Son Güncelleme</th>
-              <th scope="col">İşlemler</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="app in applications" :key="app.id">
-              <th scope="row">{{ app.id }}</th>
-              <td>{{ app.title }}</td>
-              <td>{{ app.description }}</td>
-              <td>{{ app.createDate }}</td>
-              <td>{{ app.lastUpdate }}</td>
-              <td>
-                <router-link :to="{ name: 'ApiAddOrUpdateMC', params: { app: app } }" class="btn btn-primary">Güncelle</router-link>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <v-data-table :headers="headers" :items="applications" :items-per-page="5" class="elevation-1 table-bordered text-center">
+       <template v-slot:item.actions="{ item }">
+        <router-link
+                  :to="{ name: 'ApiAddOrUpdateMC', params: { app: item } }"
+                  class="btn btn-primary"
+                >Güncelle</router-link>
+      </template>
+        </v-data-table>
       </div>
     </div>
-        <router-view/>
-
+    <router-view />
   </div>
 </template>
 
@@ -50,16 +36,66 @@ export default {
   data: () => {
     return {
       applications: [
-        { id: 1, title: 'app1', createDate: '12.03.2006', lastUpdate: '24.05.2017', description: 'app1-desc' },
-        { id: 2, title: 'app2', createDate: '12.03.2006', lastUpdate: '24.05.2017', description: 'app2-desc' },
-        { id: 3, title: 'app3', createDate: '12.03.2006', lastUpdate: '24.05.2017', description: 'app3-desc' },
-        { id: 4, title: 'app4', createDate: '12.03.2006', lastUpdate: '24.05.2017', description: 'app4-desc' },
-        { id: 5, title: 'app5', createDate: '12.03.2006', lastUpdate: '24.05.2017', description: 'app5-desc' },
-        { id: 6, title: 'app6', createDate: '12.03.2006', lastUpdate: '24.05.2017', description: 'app6-desc' }
+        {
+          id: 1,
+          title: 'app1',
+          createDate: '12.03.2006',
+          lastUpdate: '24.05.2017',
+          description: 'app1-desc'
+        },
+        {
+          id: 2,
+          title: 'app2',
+          createDate: '12.03.2006',
+          lastUpdate: '24.05.2017',
+          description: 'app2-desc'
+        },
+        {
+          id: 3,
+          title: 'app3',
+          createDate: '12.03.2006',
+          lastUpdate: '24.05.2017',
+          description: 'app3-desc'
+        },
+        {
+          id: 4,
+          title: 'app4',
+          createDate: '12.03.2006',
+          lastUpdate: '24.05.2017',
+          description: 'app4-desc'
+        },
+        {
+          id: 5,
+          title: 'app5',
+          createDate: '12.03.2006',
+          lastUpdate: '24.05.2017',
+          description: 'app5-desc'
+        },
+        {
+          id: 6,
+          title: 'app6',
+          createDate: '12.03.2006',
+          lastUpdate: '24.05.2017',
+          description: 'app6-desc'
+        }
+      ],
+      headers: [
+        {
+          text: 'Id',
+          align: 'start',
+          sortable: true,
+          value: 'id'
+        },
+        { text: 'Başlık', value: 'title' },
+        { text: 'Açıklama', value: 'description' },
+        { text: 'Oluşturma Tarihi', value: 'createDate' },
+        { text: 'Son Güncelleme', value: 'lastUpdate' },
+        { text: 'İşlemler', value: 'actions', sortable: false }
       ]
     }
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+</style>
