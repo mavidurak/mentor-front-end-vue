097d5315d575897dbc14d24045b815ee821fd072
saracalihan <saracalihan@gmail.com>
2020-08-26 02:11:10 +0300

    Kayıt olma ekranının API ile bağlanması

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -4,56 +4,82 @@
       <div class="col-md-4"></div>
       <div class="col-md-4">
         <h6>Signup Page</h6>
-          <ValidationObserver ref="form" v-slot="{ invalid }">
-        <form class="text-left" @submit.prevent="onSubmit()">
-          <div class="form-group">
-            <label for="inputMail">E-Mail :</label>
-            <validation-provider name="email" rules="required|email" v-slot="{ errors }">
-              <input
-                type="text"
-                class="form-control"
-                id="inputMail"
-                v-model="email"
-                placeholder="Mail giriniz"
-              />
+        <ValidationObserver ref="form" v-slot="{ invalid }">
+          <form class="text-left" @submit.prevent="onSubmit()">
+            <div class="form-group">
+              <label for="inputMail">E-Mail :</label>
+              <validation-provider name="email" rules="required|email" v-slot="{ errors }">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="inputMail"
+                  v-model="email"
+                  placeholder="Mail giriniz"
+                />
                 <span>{{ errors[0] }}</span>
-            </validation-provider>
-          </div>
-          <div class="form-group">
-            <label for="inputUsername">Kullanıcı Adı :</label>
-            <validation-provider name="username" rules="required|min:3|max:20" v-slot="{ errors }">
-              <input
-              type="text"
-              class="form-control"
-              id="inputUsername"
-              v-model="username"
-              placeholder="Kullanıcı adı girinz"
-            />
-            <span>{{ errors[0] }}</span>
-            </validation-provider>
+              </validation-provider>
             </div>
-          <div class="form-group">
-            <validation-provider name="password" rules="required|min:8|max:16"  v-slot="{ errors }" :bails="false">
-            <label for="inputPassword">Password</label>
-            <input
-              type="password"
-              class="form-control"
-              id="inputPassword"
-              v-model="password"
-              placeholder="Şifre giriniz"
-            />
-            <span>{{ errors[0] }}</span>
-            </validation-provider>
-          </div>
-          <button
-            type="submit"
-            name="submitButton"
-            id="submitButton"
-            class="btn btn-success float-right"
-            :disabled="invalid"
-          >Kaydol</button>
-        </form>  </ValidationObserver>
-
+            <div class="form-group">
+              <label for="inputUsername">Kullanıcı Adı :</label>
+              <validation-provider
+                name="username"
+                rules="required|min:3|max:20"
+                v-slot="{ errors }"
+              >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="inputUsername"
+                  v-model="username"
+                  placeholder="Kullanıcı adı girinz"
+                />
+                <span>{{ errors[0] }}</span>
+              </validation-provider>
+            </div>
+            <div class="form-group">
+              <label for="inputname">Ad Soyad :</label>
+              <validation-provider
+                name="name"
+                rules="required|min:3|max:20"
+                v-slot="{ errors }"
+              >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="inputname"
+                  v-model="name"
+                  placeholder="Ad Soyad  giriniz"
+                />
+                <span>{{ errors[0] }}</span>
+              </validation-provider>
+            </div>
+            <div class="form-group">
+              <validation-provider
+                name="password"
+                rules="required|min:8|max:16"
+                v-slot="{ errors }"
+                :bails="false"
+              >
+                <label for="inputPassword">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="inputPassword"
+                  v-model="password"
+                  placeholder="Şifre giriniz"
+                />
+                <span>{{ errors[0] }}</span>
+              </validation-provider>
+            </div>
+            <button
+              type="submit"
+              name="submitButton"
+              id="submitButton"
+              class="btn btn-success float-right"
+              :disabled="invalid"
+            >Kaydol</button>
+          </form>
+        </ValidationObserver>
       </div>
       <div class="col-md-4"></div>
     </div>
@@ -62,6 +88,7 @@
 
 <script>
 import { ValidationProvider, ValidationObserver } from 'vee-validate'
+import Axios from 'axios'
 
 export default {
   name: 'Signup',
@@ -74,19 +101,27 @@ export default {
       email: '',
       username: '',
       password: '',
+      name: '',
       errors: []
     }
   },
   methods: {
     onSubmit: function () {
-      console.log(
-        'Mail : ' +
-          this.email +
-          'Username: ' +
-          this.username +
-          'Password: ' +
-          this.password
-      )
+      Axios
+        .post(
+          'http://localhost:4000/authentications/register/',
+          {
+            username: this.username,
+            password: this.password,
+            email: this.email,
+            name: this.name
+          }
+        ).then(respose => {
+          if (respose.status === 201) {
+            localStorage.setItem('X-AccessToken', respose.data.token.token_value)
+            location.assign('/')
+          }
+        })
     }
   }
 }
