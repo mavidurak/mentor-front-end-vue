d877d59a369d5517a642738f613c936c04f5e071
YusufSina <yusufsinayildiz@gmail.com>
2020-09-13 15:16:35 +0300

    Sign-in and sign-up pages was developed, was translated to eng.

--- src/vee-validate.js
+++ src/vee-validate.js
@@ -1,10 +1,7 @@
-import { extend, localize } from 'vee-validate'
+import { extend } from 'vee-validate'
 import { required, email, min, max } from 'vee-validate/dist/rules'
-import tr from 'vee-validate/dist/locale/tr.json'
 
 extend('required', required)
 extend('email', email)
 extend('min', min)
 extend('max', max)
-
-localize('tr', tr)
