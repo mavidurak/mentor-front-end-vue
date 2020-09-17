14ea9a5a49ab8de9ed464c7edc302180cd3e817b
DEONSKY <bestrasen@hotmail.com>
2020-09-17 16:05:12 +0300

    profile redesign stage 0

--- src/views/Home/Profile.vue
+++ src/views/Home/Profile.vue
@@ -1,65 +1,81 @@
 <template>
   <div>
-    <Navbar/>
+    <Navbar />
     <div class="body">
-    <div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-          <div class="border my-5 py-3 shadow-lg ">
-            <div class="row mx-auto">
-              <div class="col-lg-4">
-                <v-gravatar :email="email" size="500" class="rounded img-fluid px-2" />
-              </div>
-              <div class="col-lg-8 px-5">
-                <div class="row" >
-                  <a href="#" id="spin-cog" class="rl cog-i" @click="gizleGoster('mySidenav')"><i class="fas fa-cog fa-3x closebtn mt-3"></i></a>
-                  <div id="mySidenav" class="sidenav mx-auto bg-dark d-flex">
-                    <div class="align-items-start px-3">
-                      <router-link class="rl" to="/change-nick">Nick Değiştir</router-link>
-                      <router-link class="rl" to="/change-email">Eposta Değiştir</router-link>
-                      <router-link class="rl" to="/change-password">Şifre Değiştir</router-link>
-                      <router-link class="rl" to="#">...</router-link>
+      <!--<img
+        src="https://www.wallpaperflare.com/static/471/767/754/abstract-ae-plexus-blue-dot-wallpaper.jpg"
+        style="
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+        position:absolute;"
+      />-->
+      <!--<div class="up-background"></div>-->
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="my-2">
+              <div class="row">
+                <div class="col-md-6 px-5">
+                  <div class="row">
+                    <div class="col-md-12 text-center">
+                      <v-gravatar :email="email" :size="200" class=" rounded img-fluid border shadow-lg bg-light rounded-circle border-info" />
                     </div>
                   </div>
+                  <div class="border shadow-lg rounded mt-2 px-3">
+                  <div class="row border-bottom ">
+                    <div class="col-lg-4">
+                      <h5>Username:</h5>
+                    </div>
+                    <div class="col-lg-6">
+                      <h5>{{username}}</h5>
+                    </div>
+                    <div class="col-md-2 text-right" style="font-size:15px;">
+                      <router-link to="/change-password"><i class="far fa-edit fa-2x"></i></router-link>
+                    </div>
+                  </div>
+                  <div class="row border-bottom">
+                    <div class="col-lg-4">
+                      <h5>Name Surname:</h5>
+                    </div>
+                    <div class="col-lg-6">
+                      <h5>{{name}}</h5>
+                    </div>
+                    <div class="col-md-2 text-right" style="font-size:15px;">
+                      <router-link to="/change-password"><i class="far fa-edit fa-2x"></i></router-link>
+                    </div>
+                  </div>
+                  <div class="row border-bottom ">
+                    <div class="col-lg-4">
+                      <h5>Email:</h5>
+                    </div>
+                    <div class="col-lg-6">
+                      <h5>{{email}}</h5>
+                    </div>
+                    <div class="col-md-2 text-right" style="font-size:15px;">
+                      <router-link to="/change-password"><i class="far fa-edit fa-2x"></i></router-link>
+                    </div>
+                  </div>
+                  <div class="row border-bottom ">
+                    <div class="col-lg-4">
+                      <h5>Password:</h5>
+                    </div>
+                    <div class="col-lg-6">
+                      <h5>******</h5>
+                    </div>
+                    <div class="col-md-2 text-right" style="font-size:15px;">
+                      <router-link to="/change-password"><i class="far fa-edit fa-2x"></i></router-link>
+                    </div>
+                  </div>
+                  </div>
                 </div>
-                <div class="row border-bottom pt-3">
-                  <div class="col-lg-6 pb-1">
-                    <h3>Username:</h3>
-                  </div>
-                  <div class="col-lg-6">
-                     <h3>{{username}}</h3>
-                  </div>
-                </div>
-                <div class="row border-bottom pt-3">
-                  <div class="col-lg-6">
-                     <h3>Name Surname:</h3>
-                  </div>
-                  <div class="col-lg-6">
-                     <h3>{{name}}</h3>
-                  </div>
-                </div>
-                <div class="row border-bottom pt-3">
-                  <div class="col-lg-6">
-                     <h3>Email:</h3>
-                  </div>
-                  <div class="col-lg-6">
-                     <h3>{{email}}</h3>
-                  </div>
-                </div>
+                <div class="col-md-5 border shadow-lg bg-light"></div>
               </div>
             </div>
           </div>
-            <div class="border my-5 py-3 shadow-lg " id="imf">
-              Grafik alanı
-            </div>
-            <div class="border my-5 py-3 shadow-lg " id="imf">
-              Grafik eklenecek
-            </div>
         </div>
+      </div>
     </div>
-    </div>
-  </div>
-
   </div>
 </template>
 
@@ -76,20 +92,16 @@ export default {
       username: '',
       name: '',
       email: ''
-
     }
   },
   created () {
     axios
-      .get(
-        '/authentications/me/',
-        {
-          headers: {
-            'X-AccessToken': localStorage.getItem('X-AccessToken')
-          }
+      .get('/authentications/me/', {
+        headers: {
+          'X-AccessToken': localStorage.getItem('X-AccessToken')
         }
-      )
-      .then(response => {
+      })
+      .then((response) => {
         if (response.status === 200) {
           this.username = response.data.username
           this.name = response.data.name
@@ -106,23 +118,25 @@ export default {
         secilenID.style.width = '0%'
       }
     }
-
   }
 }
-
 </script>
 
 <style scoped>
-  .fa-cog{
-    z-index: 2;
-  }
-  .ppic{
-    height:50vh;
-  }
- .row{
-  font-family:monospace;
+.up-background {
+  position: absolute;
+  height: 400px;
+  width: 100vw;
+  background: rgb(153,240,194);
+  background: linear-gradient(223deg, rgba(153,240,194,1) 0%, rgba(58,90,91,1) 50%, rgba(189,102,173,1) 100%);
+
+}
+.ppic {
+  height: 50vh;
+}
+.row {
   font-size: 20px;
-  }
+}
 
 .sidenav {
   height: 100%;
@@ -157,8 +171,7 @@ export default {
   font-size: 36px;
   margin-left: 50px;
 }
-.cog-i{
+.cog-i {
   transition: 0.5s ease;
 }
-
 </style>
