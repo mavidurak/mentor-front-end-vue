35cb2bef0b98b11de2b90158cd52bc1900c994ec
İbrahim Turan <ibrahimturan002@gmail.com>
2020-11-01 02:12:02 +0300

    login screen refactored

--- src/views/Guests/Login.vue
+++ src/views/Guests/Login.vue
@@ -1,93 +1,97 @@
 <template>
-  <div>
-    <div class="row text-center">
-      <div class="col-sm-3"></div>
-      <div class="col-sm-6">
-        <div class="text-center">
-          <img
-            src="@/assets/mavidurak-io-logo.png"
-            class="rounded"
-            id="logo"
-            alt="MaviDurak-IO Logo"
-          />
-        </div>
-        <ValidationObserver ref="form" v-slot="{ invalid }">
-          <div class="card">
-            <div class="card-body">
-              <h4>Sign In</h4>
-              <form class="text-left" @submit.prevent="onSubmit()">
-                <div class="form-group">
-                  <label for="inputUsername">Username:</label>
-                  <validation-provider name="username" rules="required" v-slot="{ errors }">
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="inputUsername"
-                      v-model="username"
-                      autocomplete="off"
-                    />
-                    <span class="text-danger">{{ errors[0] }}</span>
-                  </validation-provider>
+  <div id="login" style="height: 100vh">
+    <v-container fluid class="fill-height">
+      <v-row class="d-flex fill-heigt justify-center" align="center">
+        <v-col sm="12" md="8" lg="6">
+          <v-card>
+            <v-row class="justify-center">
+              <v-col cols="auto">
+                <img
+                  height="150"
+                  :src="logoPath"
+                  contain
+                />
+              </v-col>
+
+              <v-col class="d-flex justify-center" cols="12">
+                <h3>Login</h3>
+              </v-col>
+
+              <v-alert
+                v-if="errorMessage"
+                class="flex-grow"
+                dismissible
+                type="error"
+              >{{ errorMessage }}</v-alert>
+
+              <v-col cols="12">
+                <v-text-field
+                  v-model="username"
+                  name="username"
+                  label="Username"
+                  class="mx-3"
+                  autocomplete="false"
+                  :error="errorMessage ? true : false"
+                  prepend-icon="mdi-account"
+                ></v-text-field>
+
+                <v-text-field
+                  v-model="password"
+                  name="password"
+                  label="Password"
+                  :type="isPasswordShown ? 'text' : 'password'"
+                  class="mx-3"
+                  autocomplete="false"
+                  :error="errorMessage ? true : false"
+                  prepend-icon="mdi-key"
+                  :append-icon="isPasswordShown ? 'mdi-eye' : 'mdi-eye-off'"
+                  @click:append="isPasswordShown = !isPasswordShown"
+                ></v-text-field>
+
+                <v-col cols="12" class="px-3">
+                  <router-link to="/forgot-password" class="d-block">Forgot password</router-link>
+
+                  <span>Do you have an account?
+                    <router-link to="/signup" >Click here</router-link>
+                  </span>
+                </v-col>
+
+                <div class="d-flex mx-2">
+                  <v-btn color="success" block dark @click="onSubmit">Sign In</v-btn>
                 </div>
-                <div class="form-group">
-                  <label for="inputPassword">Password:</label>
-                  <validation-provider name="password" rules="required" v-slot="{ errors }">
-                    <input
-                      type="password"
-                      class="form-control"
-                      id="inputPassword"
-                      v-model="password"
-                    />
-                    <span class="text-danger">{{ errors[0] }}</span>
-                  </validation-provider>
-                  <router-link to="/forgot-password" class="d-block">Forgot your password?</router-link>
-                </div>
-                <div class="row justify-content-between mx-1">
-                  <router-link to="/signup">
-                    <button type="button" class="btn btn-light">Sign up</button>
-                  </router-link>
-                  <div class="block">
-                    <i id="info" v-bind:class="statusClass"></i>
-                  </div>
-                  <button
-                    type="submit"
-                    name="submitButton"
-                    id="submitButton"
-                    class="btn btn-success"
-                    :disabled="invalid"
-                  >Sign In</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </ValidationObserver>
-      </div>
-      <div class="col-sm-3"></div>
-    </div>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-import { ValidationProvider, ValidationObserver } from 'vee-validate'
 import axios from 'axios'
 import swal from 'sweetalert'
 
 export default {
   name: 'Login',
-  components: {
-    ValidationProvider,
-    ValidationObserver
-  },
   data: () => {
     return {
       username: '',
       password: '',
-      statusClass: null
+      isPasswordShown: false,
+      logoPath: require('@/assets/mavidurak-io-logo.png'),
+      errorMessage: ''
     }
   },
 
   methods: {
-    onSubmit: function () {
+    onSubmit () {
+      if (!this.username || !this.password) {
+        this.errorMessage = 'Password and username fields are required.'
+
+        return
+      }
+
       axios
         .post('/authentications/login/', {
           username: this.username,
@@ -108,7 +112,7 @@ export default {
             title: err.response.data.message,
             icon: 'error'
           })
-          console.log(err.response.data)
+
           if (err.response.data.error !== undefined) {
             swal({
               title: 'Validation Failed!',
@@ -118,20 +122,12 @@ export default {
           }
         })
     }
-  },
-
-  created: () => {
-    document.body.className = 'login-home'
   }
 }
 </script>
 
 <style>
-.login-home {
-  height: 100vh;
-  background-color: #eee;
-}
-#logo {
-  max-width: 150px;
+.justify-center {
+  justify-content: center;
 }
 </style>
